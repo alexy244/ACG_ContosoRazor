a07b627b06e11bb784d7fbb4d8a21515bd79ea84
Alexis Courtois <21811962@bucks.ac.uk>
2019-09-30 11:56:46 +0100

    Add project files.

---
+++ ACG_CoreRazorContoso/Pages/Hello.cshtml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ACG_CoreRazorContoso.Pages
+{
+    public class HelloModel : PageModel
+    {
+
+        public string Name { get; set; }
+        public void OnGet()
+        {
+            Name = null;
+        }
+
+        public void onPost()
+        {
+
+            this.Name = Request.Form["Name"];
+
+
+        }
+
+
+
+    }
+}
